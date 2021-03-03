307e9fabe08439ad8fecb3ca7332e28470b3a449
Zixian Cai <u5937495@anu.edu.au>
2021-03-04 00:30:55 +1100

    Add a default set of tests to run

--- run.py
+++ run.py
@@ -2,9 +2,21 @@
 import sys
 import subprocess
 import socket
+import time
 
 # -L -t 4 -s -I -S -o 33554432 -c 33554432
 tests = sys.argv[1:]
+if not tests:
+    tests = [
+        "-o 33554432 -t 4 -T -I -S -L -C 0", # read, baseline
+        "-o 33554432 -t 4 -T -I -S -L -C 1", # read, warmup
+        "-o 33554432 -t 4 -T -I -S -L -C 2", # read, wbinvd
+        "-o 33554432 -t 4 -T -I -S -L -C 0 -s 0", # read, reuse
+        "-o 33554432 -t 5 -T -I -S -L -C 0", # write, baseline
+        "-o 33554432 -t 5 -T -I -S -L -C 1", # write, warmup
+        "-o 33554432 -t 5 -T -I -S -L -C 2", # write, wbinvd
+        "-o 33554432 -t 5 -T -I -S -L -C 0 -s 0", # write, reuse
+    ]
 for test in tests:
     raw_args = test.split()
     args = []
@@ -16,7 +28,7 @@ for test in tests:
 
     encode_args = "".join([arg[0][1:] + arg[1] for arg in args])
 
-    log_name = "{}-{}.log".format(socket.gethostname(), encode_args)
+    log_name = "{}-{}-{}.log".format(socket.gethostname(), int(time.time()), encode_args)
     cmd = ["./rtm-bench"]
     cmd.extend(raw_args)
     print("{} > {}".format(" ".join(cmd), log_name))
